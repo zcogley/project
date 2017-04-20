e9a8cc779f90dad766f704340e01c0ffa98c92e4
Zac Cogley <zaccogley@gmail.com>
2017-04-20 09:00:53 -0500

    events adding to model and rendering

--- scripts/today.js
+++ scripts/today.js
@@ -1,33 +1,59 @@
 
 
-// MODEL
-
+// ----------MODEL---------
 var model = {
 
   events: [],
 
 };
 
+// ----------VIEW----------
+function render() {
+  // updates the event list based on the model
+  $('#events').empty();
+
+  var events = model.events.map(eventElement);
+  $('#events').append(events);
+
+  // clears the eventbox
+  $('eventbox').val('');
+
+  // gives focus to the eventbox
+  $('eventbox').focus();
+
+}
 
 
+// adds a new event
 function addNewEvent(event){
   model.events.push({event});
 }
 
 
-// when the textbox content changes, updates the .currentEvent
-// property of the model
-$('#eventbox').on('input', () => {
-  model.currentEvent = $('#eventbox').val();
-  console.log(model.currentEvent);
-});
 
-// when the form is submitted
-$('#add-event-form').submit((evt) => {
-  // don't refresh the page
-  evt.preventDefault();
+// creates an event element
+function eventElement(item) {
+  var eventElement = $('<li></li>').text(item.event);
+  return eventElement;
+}
 
-  // add a new event from whatever typed
-  addNewEvent(model.currentEvent);
+// ---------DOM EVENT HANDLERS---------
+$(document).ready(() => {
+  // when the textbox content changes, updates the .currentEvent
+  // property of the model
+  $('#eventbox').on('input', () => {
+    model.currentEvent = $('#eventbox').val();
+  });
 
-});
+  // when the form is submitted
+  $('#add-event-form').submit((evt) => {
+    // don't refresh the page
+    evt.preventDefault();
+
+    // add a new event from whatever typed
+    addNewEvent(model.currentEvent);
+
+    // renders page
+    render();
+  });
+})
